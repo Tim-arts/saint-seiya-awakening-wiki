495be69dbeda5c53cc201e864f59d96ed0036092
Tim.art <tyrion.graphiste@gmail.com>
2020-02-23 22:46:51 -0800

    Update paths.

--- server/transform.js
+++ server/transform.js
@@ -1,6 +1,14 @@
 const fs = require("fs");
 const browserify = require("browserify");
-const folders = ["js/front/", "js/back/", "js/front/views/", "js/back/views/"];
+const folders = [
+    "js/front/",
+    "js/back/",
+    
+    "js/front/views/",
+    "js/back/views/",
+    
+    "js/front/views/saints/"
+];
 
 folders.forEach(folder => {
     fs.readdirSync(folder, {withFileTypes: true})
